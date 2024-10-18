f6b425e2a9006a917974ca8f7bc81658e5546f8a
Jasmine <truc.h.nguyen2@boeing.com>
2024-10-17 22:05:43 -0700

    completed homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import '../styles/header.css'
+import '../styles/font.css'
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
